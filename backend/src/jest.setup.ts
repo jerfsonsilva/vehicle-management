c4278ffd354ea8163a1f6db438c6e3d7a98f68eb
jerfsonlink <jerfsonlink@gmail.com>
2026-04-08 14:05:40 -0300

    to config jest to do not show logs

---
+++ backend/src/jest.setup.ts
@@ -0,0 +1,13 @@
+import { Logger } from '@nestjs/common';
+
+beforeEach(() => {
+  jest.spyOn(Logger.prototype, 'log').mockImplementation();
+  jest.spyOn(Logger.prototype, 'warn').mockImplementation();
+  jest.spyOn(Logger.prototype, 'error').mockImplementation();
+  jest.spyOn(Logger.prototype, 'debug').mockImplementation();
+  jest.spyOn(Logger.prototype, 'verbose').mockImplementation();
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
